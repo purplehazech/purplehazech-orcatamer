4adc67ee1e2a27f01bb24709c40d98e823e378bc
Lucas Bickel <hairmare@purplehaze.ch>
2014-01-12 08:54:17 +0100

    carry over parts of peewee config

--- manifests/default.pp
+++ manifests/default.pp
@@ -15,19 +15,57 @@ node default {
       ensure  => present,
       # enable local overlay (this is a dev box after all)
       content => '/usr/local/portage';
+    'source /usr/local/portage/make.conf':
+      ensure => present;
     'features':
       ensure  => present,
       content => [
         'sandbox',
+        'parallel-fetch',
         # activate binary package building
         'buildpkg',
         'buildsyspkg',
         # use binary packages when available
         'getbinpkg',
       ];
+    'use':
+      ensure  => present,
+      content => [
+        'nls',
+        'cjk',
+        'unicode'
+      ];
     'pkgdir':
       ensure  => present,
-      content => '/vagrant/portage/packages',
+      content => '/vagrant/portage/packages';
+    'python_targets':
+      ensure  => present,
+      content => [
+        'python2_7',
+        'python3_2',
+        'python3_3',
+      ];
+    'use_python':
+      ensure  => present,
+      content => [
+        '3.2',
+        '2.7',
+      ];
+    'linguas':
+      ensure  => present,
+      content => [
+        'en',
+      ];
+    'input_devices':
+      ensure  => present,
+      content => [
+        'evdev',
+      ];
+    'video_cards':
+      ensure  => present,
+      content => [
+        'virtualbox',
+      ];
   } ->
   # install layman since we'll use him anyway
   package { 'layman':
