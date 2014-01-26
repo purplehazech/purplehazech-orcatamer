5337e3d3749d219cb681859b10c727b06a37e248
Lucas Bickel <hairmare@purplehaze.ch>
2014-01-27 00:58:43 +0100

    some more ordering fixes

--- manifests/profile/manifests/puppet/master.pp
+++ manifests/profile/manifests/puppet/master.pp
@@ -125,7 +125,15 @@ class profile::puppet::master {
   file { '/etc/nginx/conf.d':
     ensure => directory
   } ->
-  class { 'nginx': }
+  class { 'nginx': } ->
+  exec { 'nginx-add-conf.d':
+    command => '/bin/sed --in-place -e "s@include /etc/nginx/mime.types;@include /etc/nginx/mime.types;\n\tinclude /etc/nginx/conf.d/*.conf;@" /etc/nginx/nginx.conf',
+    unless  => '/bin/grep "include /etc/nginx/conf.d/\*.conf;" /etc/nginx/nginx.conf',
+  } ~>
+  exec { 'restart-nginx-after-conf':
+    command     => '/etc/init.d/nginx restart',
+    refreshonly => true
+  }
 
   nginx::resource::upstream { 'puppetboard':
     ensure  => present,
@@ -181,8 +189,12 @@ class profile::puppet::master {
     target => '/etc/init.d/uwsgi',
   } ->
   service { 'uwsgi.puppetboard':
-    ensure => running,
-    enable => true
+    ensure  => running,
+    enable  => true,
+    require => [
+      Class['nginx'],
+      Service['puppetmaster']
+    ]
   }
 
   # puppetmaster
