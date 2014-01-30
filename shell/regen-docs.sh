244ac5efff63e763bb628173cf4bc45d9a78694e
Lucas Bickel <hairmare@purplehaze.ch>
2014-01-30 22:04:49 +0100

    docs generator

---
+++ shell/regen-docs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+find 	manifests/site.pp \
+	manifests/nodes/ \
+	manifests/role/ \
+	manifests/profile/ \
+	-name '*.pp' \
+	-exec grep '#' {} \; | \
+sed -e 's/[ ]*#//' | \
+awk '/^#/ {print "\n"} {print $0} ' - \
+> "`dirname $0`/../Puppetdoc.md" && \
+git rev-parse --verify HEAD \
+> "`dirname $0`/../Puppetdoc.lock"
